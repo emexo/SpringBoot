00155e2d5b676fc561dae5d453b19759e3ed60d0
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2024-02-10 21:04:25 +0530

    Update CustomerService.java

--- bankingmanagement-api/src/main/java/com/bankingmanagement/service/CustomerService.java
+++ bankingmanagement-api/src/main/java/com/bankingmanagement/service/CustomerService.java
@@ -4,8 +4,13 @@ import com.bankingmanagement.exception.CustomerNotFoundException;
 import com.bankingmanagement.model.CustomerDTO;
 import com.bankingmanagement.model.CustomerRequest;
 
+import java.util.List;
+
 
 public interface CustomerService {
+
+    List<CustomerDTO> findAll() throws CustomerNotFoundException;
+
     CustomerDTO findCustomerById(int customerId) throws CustomerNotFoundException;
 
     CustomerDTO saveCustomer(CustomerRequest customerRequest) throws IllegalArgumentException, CustomerNotFoundException;
