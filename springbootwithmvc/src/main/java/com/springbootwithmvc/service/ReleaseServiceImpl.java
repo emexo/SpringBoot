8f2b2eb90ec5ee1dc73e8b2f729228aa9b153c1a
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2024-02-20 06:41:09 +0530

    Add files via upload

--- springbootwithmvc/src/main/java/com/springbootwithmvc/service/ReleaseServiceImpl.java
+++ springbootwithmvc/src/main/java/com/springbootwithmvc/service/ReleaseServiceImpl.java
@@ -2,6 +2,7 @@ package com.springbootwithmvc.service;
 
 import com.springbootwithmvc.entity.Release;
 import com.springbootwithmvc.exception.ReleaseDetailsNotFoundException;
+import com.springbootwithmvc.model.ReleaseRequest;
 import com.springbootwithmvc.model.ReleaseTO;
 import com.springbootwithmvc.repository.ReleaseRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,4 +42,26 @@ public class ReleaseServiceImpl implements ReleaseService{
         log.info("End of ReleaseServiceImpl.findAllReleases");
         return releaseTOS;
     }
+
+    @Override
+    public ReleaseTO saveRelease(ReleaseRequest request) throws ReleaseDetailsNotFoundException {
+        log.info("Inside the ReleaseServiceImpl.saveRelease,request;{} ", request);
+        Release release = new Release();
+        release.setReleaseDate(request.getReleaseDate());
+        release.setDescription(request.getDescription());
+        Release release1 = releaseRepository.save(release);
+
+        if(Objects.isNull(release1)){
+            log.error("Release details not saved");
+            throw new ReleaseDetailsNotFoundException("Release details not saved");
+        }
+
+        ReleaseTO releaseTO = new ReleaseTO();
+        releaseTO.setId(release1.getId());
+        releaseTO.setReleaseDate(release1.getReleaseDate());
+        releaseTO.setDescription(release1.getDescription());
+        return releaseTO;
+    }
+
+
 }
