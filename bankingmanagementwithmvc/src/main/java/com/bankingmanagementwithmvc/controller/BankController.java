a2b99aa8e594ffb0371dfc12d492110737b67fef
eMexo Technologies <49759592+emexo@users.noreply.github.com>
2024-08-05 07:02:58 +0530

    Add files via upload

--- bankingmanagementwithmvc/src/main/java/com/bankingmanagementwithmvc/controller/BankController.java
+++ bankingmanagementwithmvc/src/main/java/com/bankingmanagementwithmvc/controller/BankController.java
@@ -1,15 +1,20 @@
 package com.bankingmanagementwithmvc.controller;
 
+import com.bankingmanagementwithmvc.exception.BankDetailsNotFound;
+import com.bankingmanagementwithmvc.model.BankRequest;
 import com.bankingmanagementwithmvc.model.BankTO;
+import com.bankingmanagementwithmvc.model.BankUpdateRequest;
 import com.bankingmanagementwithmvc.service.BankService;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -21,11 +26,63 @@ public class BankController {
     public BankService bankService;
 
     @GetMapping("/list")
-    public String findAll(Model model) throws Exception {
+    public String findAll(Model model) {
         log.info("Inside the BankControllerV2.findAll");
-        List<BankTO> bankTOS = bankService.findAll();
+        List<BankTO> bankTOS = null;
+        try {
+            bankTOS = bankService.findAll();
+        } catch (BankDetailsNotFound e) {
+            model.addAttribute("error", "Bank Details not Found");
+        }
         model.addAttribute("banks", bankTOS);
         log.info("End of BankControllerV2.findAll");
         return "banks";
     }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        log.info("Inside the BankControllerV2.create");
+        BankRequest bank = new BankRequest();
+        model.addAttribute("bank", bank);
+        return "createbank";
+    }
+
+    @PostMapping("/save")
+    public RedirectView save(@ModelAttribute("bank") BankRequest bank) throws BankDetailsNotFound {
+        bankService.save(bank);
+
+        RedirectView redirectView = new RedirectView();
+        redirectView.setContextRelative(true);
+        redirectView.setUrl("/banks/list");
+        return redirectView;
+    }
+
+    @GetMapping("/delete/{bankCode}")
+    public RedirectView delete(@PathVariable("bankCode") int bankCode) throws BankDetailsNotFound {
+        bankService.delete(bankCode);
+
+        RedirectView redirectView = new RedirectView();
+        redirectView.setContextRelative(true);
+        redirectView.setUrl("/banks/list");
+        return redirectView;
+    }
+
+    @GetMapping("/edit/{bankCode}")
+    public String update(@PathVariable("bankCode") int bankCode, Model model) throws BankDetailsNotFound {
+        log.info("Inside the BankControllerV2.create");
+        BankTO bankTO = bankService.findById(bankCode);
+        model.addAttribute("bank", bankTO);
+        return "updatebank";
+    }
+
+    @PostMapping("/update")
+    public RedirectView update(@ModelAttribute("bank") BankUpdateRequest bank) throws BankDetailsNotFound {
+        bankService.update(bank);
+
+        RedirectView redirectView = new RedirectView();
+        redirectView.setContextRelative(true);
+        redirectView.setUrl("/banks/list");
+        return redirectView;
+    }
+
 }
--- bankingmanagementwithmvc/src/main/java/com/bankingmanagementwithmvc/service/BankServiceImpl.java
+++ bankingmanagementwithmvc/src/main/java/com/bankingmanagementwithmvc/service/BankServiceImpl.java
@@ -2,7 +2,9 @@ package com.bankingmanagementwithmvc.service;
 
 import com.bankingmanagementwithmvc.entity.Bank;
 import com.bankingmanagementwithmvc.exception.BankDetailsNotFound;
+import com.bankingmanagementwithmvc.model.BankRequest;
 import com.bankingmanagementwithmvc.model.BankTO;
+import com.bankingmanagementwithmvc.model.BankUpdateRequest;
 import com.bankingmanagementwithmvc.repository.BankRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +12,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class BankServiceImpl implements BankService{
     @Autowired
-    public BankRepository bankMongoRepository;
+    public BankRepository bankRepository;
 
     @Override
     public List<BankTO> findAll() throws BankDetailsNotFound {
         log.info("Inside the BankServiceImplV2.findAllBank");
-        List<Bank> banks = bankMongoRepository.findAll();
+        List<Bank> banks = bankRepository.findAll();
 
         log.info("List of Banks:{}", banks);
         if(CollectionUtils.isEmpty(banks)){
@@ -39,5 +43,97 @@ public class BankServiceImpl implements BankService{
 
         log.info("End of BankServiceImplV2.findAllBank");
         return bankTOS;
+    }
+
+    @Override
+    public BankTO save(BankRequest bankRequest) throws BankDetailsNotFound {
+        log.info("Inside the BankServiceImpl.save, bankRequest:{}", bankRequest);
+        Bank bank = new Bank();
+        bank.setBankCode(bankRequest.getBankCode());
+        bank.setBankName(bankRequest.getBankName());
+        bank.setBankAddress(bankRequest.getBankAddress());
+        Bank response = bankRepository.save(bank);
+
+        if(Objects.isNull(response)){
+            log.error("Bank details not saved");
+            throw new BankDetailsNotFound("Bank details not saved");
+        }
+        // apply the business logic
+        BankTO bankTO = new BankTO();
+        bankTO.setBankName(response.getBankName());
+        bankTO.setBankAddress(response.getBankAddress());
+
+        log.info("End of BankServiceImpl.save");
+        return bankTO;
+    }
+
+    @Override
+    public BankTO update(BankUpdateRequest bankRequest) throws BankDetailsNotFound {
+        log.info("Inside the BankServiceImpl.save, bankRequest:{}", bankRequest);
+        Optional<Bank> bankOptional = bankRepository.findById(bankRequest.getBankCode());
+
+        if(bankOptional.isEmpty()){
+            log.error("Bank details not found");
+            throw new BankDetailsNotFound("Bank details not found");
+        }
+        Bank bank = bankOptional.get();
+        bank.setBankCode(bankRequest.getBankCode());
+        if(Objects.nonNull(bankRequest.getBankCode())) {
+            bank.setBankCode(bankRequest.getBankCode());
+        }
+
+        if(Objects.nonNull(bankRequest.getBankName())) {
+            bank.setBankName(bankRequest.getBankName());
+        }
+        if(Objects.nonNull(bankRequest.getBankAddress())) {
+            bank.setBankAddress(bankRequest.getBankAddress());
+        }
+        Bank response = bankRepository.save(bank);
+
+        if(Objects.isNull(response)){
+            log.error("Bank details not saved");
+            throw new BankDetailsNotFound("Bank details not saved");
+        }
+        // apply the business logic
+        BankTO bankTO = new BankTO();
+        bankTO.setBankName(response.getBankName());
+        bankTO.setBankAddress(response.getBankAddress());
+
+        log.info("End of BankServiceImpl.save");
+        return bankTO;
+    }
+
+    @Override
+    public String delete(int id) throws BankDetailsNotFound {
+        log.info("Inside the BankServiceImpl.delete, id:{}", id);
+        Optional<Bank> bankOptional = bankRepository.findById(id);
+
+        if(bankOptional.isEmpty()){
+            log.error("Bank details not found");
+            throw new BankDetailsNotFound("Bank details not found");
+        }
+        bankRepository.deleteById(id);
+        log.info("End of BankServiceImpl.delete");
+        return "Bank detail has been deleted";
+    }
+
+    @Override
+    public BankTO findById(int id) throws BankDetailsNotFound {
+        log.info("Inside the BankServiceImpl.findById, id:{}", id);
+        Optional<Bank> bankOptional = bankRepository.findById(id);
+
+        if(bankOptional.isEmpty()){
+            log.error("Bank details not found");
+            throw new BankDetailsNotFound("Bank details not found");
+        }
+        // apply the business logic
+        Bank bank = bankOptional.get();
+        BankTO bankTO = new BankTO();
+        bankTO.setBankCode(bank.getBankCode());
+        bankTO.setBankName(bank.getBankName());
+        bankTO.setBankAddress(bank.getBankAddress());
+
+        log.info("End of BankServiceImpl.findAll");
+        return bankTO;
     }
 }
